5a038c175683cd0d1368ec6cc30603cf5ac774ba
garsue <labs.garsue@gmail.com>
2017-01-16 13:52:45 +0900

    Add m flag and v flag

--- main.go
+++ main.go
@@ -12,6 +12,10 @@ import (
 
 type users []string
 
+var (
+	verbose *bool
+)
+
 func (u *users) String() string {
 	return fmt.Sprint(*u)
 }
@@ -22,17 +26,21 @@ func (u *users) Set(value string) error {
 }
 
 func main() {
+	// Load command line options
 	var users users
 	flag.Var(&users, "u", "Set a notified user ID")
+	msg := flag.String("m", "", "Set a message")
 	fpath := flag.String("f", "", "Set a comment file")
+	verbose = flag.Bool("v", false, "Verbose mode")
 	flag.Parse()
-	if fpath == nil || len(*fpath) == 0 {
-		log.Fatalln("Must set a comment file")
-	}
+
+	// Get setting from env vars
 	domain := os.Getenv("BACKLOG_DOMAIN")
 	spaceID := os.Getenv("BACKLOG_SPACE_ID")
 	issue := os.Getenv("BACKLOG_ISSUE")
 	apiKey := os.Getenv("BACKLOG_API_KEY")
+
+	// Compose a request
 	endpoint := fmt.Sprintf(
 		"https://%s.%s/api/v2/issues/%s/comments?apiKey=%s",
 		spaceID,
@@ -41,16 +49,17 @@ func main() {
 		apiKey,
 	)
 	data := url.Values{}
-	comment, err := loadComment(*fpath)
-	log.Println(comment)
+	comment, err := makeCommentText(msg, fpath)
 	if err != nil {
 		log.Fatalln(err)
 	}
+	debug(comment)
 	data.Set("content", comment)
 	for _, user := range users {
 		data.Add("notifiedUserId[]", user)
 	}
 
+	// Post
 	resp, err := http.PostForm(endpoint, data)
 	if err != nil {
 		log.Fatalln(err)
@@ -62,13 +71,26 @@ func main() {
 	if err != nil {
 		log.Fatalln(err)
 	}
-	fmt.Print(string(body))
+	debug(string(body))
 }
 
-func loadComment(fpath string) (string, error) {
-	bytes, err := ioutil.ReadFile(fpath)
+func makeCommentText(msg, fpath *string) (string, error) {
+	var comment string
+	if msg != nil {
+		comment = *msg
+	}
+	if fpath == nil {
+		return comment, nil
+	}
+	bytes, err := ioutil.ReadFile(*fpath)
 	if err != nil {
 		return "", err
 	}
-	return string(bytes), nil
+	return comment + "\n" + string(bytes), nil
+}
+
+func debug(format string, v ...interface{}) {
+	if verbose != nil && *verbose {
+		log.Printf(format, v)
+	}
 }
